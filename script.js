7c5587d41844d3707a0149a9a72629d5d3b817b5
winishasmith <winishasmith@gmail.com>
2016-02-22 16:26:44 -0500

    added for loop for questions array

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var button = document.querySelector("button");
-var questions = ["How many continents are there?"]
+var questions = [
+  "How many continents are there?",
+  "He was the 16th U.S. President.",
+  "This James Cameron film, released in 2009, is considered the highest grossing movie of all time.",
+  "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter."]
 
 button.addEventListener ("click", function(){
-  var answer = prompt (questions[0]);
-  console.log (answer);
+  for (var i = 0; i < questions.length; i++){
+    var answer = prompt (questions[i]);
+    console.log (answer);
+  }
+
 });
