cb9e466b98ae78a2eef0388c35fb6b4d46c64ccc
winishasmith <winishasmith@gmail.com>
2016-02-23 12:45:31 -0500

    each question added as an object within array

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var button = document.querySelector("button");
 var questions = [
-  "How many continents are there?",
-  "He was the 16th U.S. President.",
-  "This James Cameron film, released in 2009, is considered the highest grossing movie of all time.",
-  "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter."]
+  {question: "How many continents are there?", answer: 7},
+  {question: "He was the 16th U.S. President.", answer: "Abraham Lincoln"},
+  {question: "This James Cameron film, released in 2009 is considered the highest grossing movie of all time.", answer: "Avatar"},
+  {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia"}
+]
 
 button.addEventListener ("click", function(){
   for (var i = 0; i < questions.length; i++){
-    var answer = prompt (questions[i]);
+    var answer = prompt (questions.question);
     console.log (answer);
   }
 
