f23cbadae667a3d679f582dd0f195c8730f4820c
winishasmith <winishasmith@gmail.com>
2016-02-23 13:48:27 -0500

    added 'counter' div, new variables for correct and incorrect answers, and if statement to event listener function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var button = document.querySelector("button");
+var counter = document.getElementById("counter");
+var correctAnswers = 0;
+var incorrectAnswers = 0;
 var questions = [
   {question: "How many continents are there?", answer: 7},
   {question: "He was the 16th U.S. President.", answer: "Abraham Lincoln"},
@@ -6,10 +9,17 @@ var questions = [
   {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia"}
 ]
 
-button.addEventListener ("click", function(){
-  for (var i = 0; i < questions.length; i++){
-    var answer = prompt (questions.question);
-    console.log (answer);
-  }
 
+button.addEventListener ("click", function() {
+    for (var i = 0; i < questions.length; i++) {
+      var answer = prompt (questions[i].question);
+        if (answer == questions[i].answer) {
+        correctAnswers++
+        counter.innerHTML = ("Great job! You've answered " + correctAnswers + " questions correctly.")
+        }
+        else {
+          incorrectAnswers++
+          counter.innerHTML = ("Sorry that's the wrong answer. You've answered " + incorrectAnswers + " questions incorrectly")
+        }
+    }
 });
