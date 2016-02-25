25f1f3bffa5b782f2dd9fc2dedabc6ce971a094b
winishasmith <winishasmith@gmail.com>
2016-02-24 23:46:55 -0500

    updated movie questions

--- script.js
+++ script.js
@@ -4,39 +4,39 @@ var incorrectAnswers = 0;
 var questions = [
   {value: "U.S. States"},
   {value: "Music"},
-  {value: "In the Movies"},
+  {value: "Famous Movie Quotes (name the movie)"},
   {value: "Food"},
   {value: "Popular Phrases"},
 
   {question: "Home of the Grand Canyon National Park", answer: "Arizona", value: 100},
   {question: "He was the 16th U.S. President.", answer: "Abraham Lincoln", value: 100},
-  {question: "This James Cameron film, released in 2009 is considered the highest grossing movie of all time.", answer: "Avatar", value: 100},
+  {question: " 'To infinity and beyond...' ", answer: "Toy Story", value: 100},
   {question: "To be embarrased is to have this 'on one\'s face'.", answer: "egg", value: 100},
   {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia", value: 100},
 
 
   {question: "This states name begins with two vowels.", answer: "Iowa", value: 200},
   {question: "He was the 16th U.S. President.", answer: "Abraham Lincoln", value: 200},
-  {question: "This James Cameron film, released in 2009 is considered the highest grossing movie of all time.", answer: "Avatar", value: 200},
+  {question: "Tom Cruise said 'show me the money' in this 1996 film.", answer: "Avatar", value: 200},
   {question: "Hot out of the oven is one of these stuffed pizza turnovers of Naples.", answer: "calzone", value: 200},
   {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia", value: 200},
 
   {question: "In 1968, gold was officially named this state's mineral.", answer: "Alaska", value: 300},
   {question: "He was the 16th U.S. President.", answer: "Abraham Lincoln", value: 300},
-  {question: "This James Cameron film, released in 2009 is considered the highest grossing movie of all time.", answer: "Avatar", value: 300},
+  {question: " 'Mama always said life was like a box of chocolates. You never know what you're gonna get.' ", answer: "Forest Gump", value: 300},
   {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia", value: 300},
   {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia", value: 300},
 
   {question: "Around 1,000 official reports of UFOs are made each year in this state.", answer: "New Mexico", value: 400},
   {question: "He was the 16th U.S. President.", answer: "Abraham Lincoln", value: 400},
-  {question: "This James Cameron film, released in 2009 is considered the highest grossing movie of all time.", answer: "Avatar", value: 400},
+  {question: " 'This is Sparta!' ", answer: 300, value: 400},
   {question: "Some recipes for this sweet treat call for beets instead of food coloring.", answer: "red velvet cake", value: 400},
   {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia", value: 400},
 
 
   {question: "This state was the first state to ratify the U.S.constitution. It did so on December 7, 1787.", answer: "Delaware", value: 500},
   {question: "He was the 16th U.S. President.", answer: "Abraham Lincoln", value: 500},
-  {question: "This James Cameron film, released in 2009 is considered the highest grossing movie of all time.", answer: "Avatar", value: 500},
+  {question: " 'Why so serious?' ", answer: "The Dark Knight", value: 500},
   {question: "This brand offers a big bean dispenser with up to 20 assorted flavors", answer: "jelly belly", value: 500},
   {question: "This state is the number one peanut producer in the U.S. and half of its crop gets processed into peanut butter.", answer: "Georgia", value: 500},
 ]
